3b0825548134e6b4572ba929789dcc365c888fda
Validark <Validarkness@gmail.com>
2019-01-24 02:22:10 -0600

    Upgrade
    - Add and fix comments
    - Make less rigid property parsers more rigid
    - Remove unused code
    - Detect invalid File characters which can cause problems
    - Consolidate duplicated code into reusing functions
    - Update assert calls to `assertTag` and `assertType`
    - Silence Lualinter

--- RBXport.lua
+++ RBXport.lua
@@ -21,11 +21,16 @@ local RBXRefreshTask = [[{
 }
 ]]
 
+local INVALID_FILE_CHARS = "[/\\:*?\"|<>]"
+
+-- Makes the Folder if it doesn't exist
+-- For whatever reason, still complains if it does exist
 local function MakeFolder(Location)
 	Location = "\"" .. Location .. "\""
 	os.execute("IF NOT exist " .. Location .. "/nul ( mkdir " .. Location .. " )")
 end
 
+-- Returns the Source at Directory
 local function GetFileSource(Directory)
 	local Original = assert(io.open(Directory, "r"))
 	local Source = Original:read("*all")
@@ -33,8 +38,9 @@ local function GetFileSource(Directory)
 	return Source
 end
 
+-- Creates a File at Location with Source
+-- Creates the Folder it should be in if it doesn't exist
 local function WriteToFile(Location, Source)
-	-- local File = assert(io.open(Location, "w"))
 	MakeFolder(Location:gsub("\\[^\\]+$", ""))
 	local Success, File = pcall(io.open, Location, "w")
 
@@ -55,6 +61,7 @@ local function InvertTable(t)
 	return o
 end
 
+-- Creates the Variables necessary to convert back and forth to HTML escape/unescape
 local EscapableString, HTMLEscapes, UnescapableString, HTMLUnescapes do
 	-- String:gsub(EscapableString, HTMLEscapes)
 	-- String:gsub(UnescapableString, HTMLUnescapes)
@@ -90,6 +97,7 @@ local EscapableString, HTMLEscapes, UnescapableString, HTMLUnescapes do
 	HTMLUnescapes = InvertTable(HTMLEscapes)
 end
 
+-- Helper for MatchBalanced to know what position to skip to
 local CaptureStarts = setmetatable({}, {
 	__index = function(self, i)
 		self[i] = i:find("%b", 1, true) - 1
@@ -97,15 +105,19 @@ local CaptureStarts = setmetatable({}, {
 	end
 })
 
+-- Helper function which basically captures the inside of a %b capture
+	-- assumes anchoring to the front of the String (at Pos)
+	-- assumes a single capture
 local function MatchBalanced(Str, Pattern, Pos, Bool)
 	local a, b = Str:find("^" .. Pattern, Pos, Bool)
 	if a then return Str:sub(CaptureStarts[Pattern] + a + 1, b - 1), a, b end
 end
 
+-- Gets the NextTag for a given Position
 local function NextTag(Source, Position)
 	local _, e = Source:find("%s*", Position + 1)
 	Position = (e or Position) + 1
-	local a, b, Str = Source:find("^<!%[CDATA%[(.-)%]%]>", Position) -- Handling ProtectedStrings can be complicated
+	local a, b, Str = Source:find("^<!%[CDATA%[(.-)%]%]>", Position) -- Handling ProtectedStrings can be complicated, so we use a special case
 
 	if a then
 		return b, Str, "Tag"
@@ -123,6 +135,17 @@ local function NextTag(Source, Position)
 	end
 end
 
+--[[
+Returns our stateful Next() function
+Type is a string: either "Tag" or "Literal"
+Value is a string which is whatever it is
+
+Example parse:
+<code>4</code>
+1: Next() - "code", "Tag"
+2: Next() - "4", "Literal"
+3: Next() - "/code", "Tag"
+--]]
 local function GetXMLParser(Directory)
 	return coroutine.wrap(function()
 		for _, Value, Type in NextTag, GetFileSource(Directory), 0 do
@@ -139,12 +162,25 @@ local Ending = {
 	StringValue = ".txt";
 }
 
+local function CompileErrorData(Location, Object)
+	local PropertyCompile = {}
+	for i, v in next, Object do
+		table.insert(PropertyCompile, tostring(i) .. " = " .. tostring(v))
+	end
+	return "\n\nLocation: " .. Location .. "\nProperties: " .. "{\n\t" .. table.concat(PropertyCompile, ";\n\t") .. "\n}\n"
+end
+
 local function BuildProject(Directory, Tree)
 	local MadeStuff = false
 
 	local function CallOnChildren(Location, Object)
 		-- Instantiate Object at Location
 		local Name = Object.Name
+
+		if not Name or Name:find(INVALID_FILE_CHARS) then
+			error(tostring(Name) .. " is an invalid name and cannot be written to a local file." .. CompileErrorData(Location, Object))
+		end
+
 		local ClassName = Object.ClassName
 		local Children = Object.Children
 		local IsScript = ClassName == "Script" or ClassName == "LocalScript" or ClassName == "ModuleScript"
@@ -169,10 +205,12 @@ local function BuildProject(Directory, Tree)
 	end
 end
 
+-- Helper error function
 local function Unexpected(Expected, Value, Type)
 	error("Expected " .. Expected .. ", got " .. Type .. " " .. Value, 3)
 end
 
+-- Makes sure a (Value, Type) is of the ExpectedType
 local function assertType(ExpectedType, Value, Type)
 	if Type ~= ExpectedType then
 		Unexpected(ExpectedType, Value, Type)
@@ -180,13 +218,7 @@ local function assertType(ExpectedType, Value, Type)
 	return Value, Type
 end
 
-local function assertValue(ExpectedValue, Value, Type)
-	if Value ~= ExpectedValue then
-		Unexpected(ExpectedValue, Value, Type)
-	end
-	return Value, Type
-end
-
+-- Makes sure a (Value, Type) is a Tag and is the ExpectedTag
 local function assertTag(ExpectedTag, Value, Type)
 	if Type ~= "Tag" then
 		Unexpected("Tag", Value, Type)
@@ -196,15 +228,7 @@ local function assertTag(ExpectedTag, Value, Type)
 	return Value, Type
 end
 
-local function assertLiteral(ExpectedLiteral, Value, Type)
-	if Type ~= "Literal" then
-		Unexpected("Literal", Value, Type)
-	elseif Value ~= ExpectedLiteral then
-		Unexpected(ExpectedLiteral, Value, Type)
-	end
-	return Value, Type
-end
-
+-- Expects a single literal, with no literal meaning nil
 local function SingleLiteral(Next, Closer)
 	local Value, Type = Next()
 
@@ -217,7 +241,8 @@ local function SingleLiteral(Next, Closer)
 			assertTag(Closer, Next())
 		end
 	end
-	return Value
+
+	return Value, Type
 end
 
 local function ExpectFields(DataType, t)
@@ -239,35 +264,33 @@ local function ExpectFields(DataType, t)
 	end
 end
 
-local function ExpectSpaceSeparatedLiterals(DataType)
-	return function(Next, Closer)
-		local Value, Type = Next()
-		assertType("Literal", Value, Type)
+local function GetSpaceSeparatedLiterals(Next, Closer)
+	local Value, Type = Next()
+	assertType("Literal", Value, Type)
 
-		local Parameters = {}
+	local Parameters = {}
 
-		for Num in Value:gmatch("%S+") do
-			table.insert(Parameters, Num)
-		end
-
-		assertTag(Closer, Next())
-		return DataType .. ".new(" .. table.concat(Parameters, ", ") .. ")"
+	for Num in Value:gmatch("%S+") do
+		table.insert(Parameters, Num)
 	end
+
+	assertTag(Closer, Next())
+	return Parameters
 end
 
+-- The table which contains functions which process different Types
+-- Functions take in `(Next, Closer)`.
+-- `Closer` is a string, which is the Tag that opened the function, but with a "/" added at the beginning
+-- This helps me reuse functions like SingleLiteral.
+-- `Next` is a stateful function which advances the XML parser.
+	-- It returns `Value, Type`, where `Type` is "Tag" or "Literal" and Value is a string of whatever the parser is currently on,
+	-- excluding the signs `<>` surrounding signs.
 local TypeProcess = setmetatable({
 	ProtectedString = function(Next, Closer)
-		local Value, Type = Next()
+		local Value, Type = SingleLiteral(Next, Closer)
 
 		if Type == "Literal" then
-			assertTag(Closer, Next())
 			Value = Value:gsub(UnescapableString, HTMLUnescapes)
-		else
-			if Value == Closer then
-				Value = nil
-			else
-				assertTag(Closer, Next())
-			end
 		end
 
 		return Value
@@ -289,25 +312,25 @@ local TypeProcess = setmetatable({
 	CoordinateFrame = ExpectFields("CFrame", {"X", "Y", "Z", "R00", "R01", "R02", "R10", "R11", "R12", "R20", "R21", "R22"});
 	Faces = ExpectFields("Faces", {"faces"});
 
-	Color3uint8 = function(Next)
-		local B = tonumber((Next()))
+	Color3uint8 = function(Next, Closer)
+		local B = tonumber((assertType("Literal", Next())))
 		local _ = (B - B % 0x01000000) / 0x01000000
 		B = B - _ * 0x01000000
 		local R = (B - B % 0x00010000) / 0x00010000
 		B = B - R * 0x00010000
 		local G = (B - B % 0x00000100) / 0x00000100
 		B = B - G * 0x00000100
-		Next()
+		assertTag(Closer, Next())
 		return "Color3.fromRGB(" .. R .. ", " .. G .. ", " .. B .. ")"
 	end;
 
 	PhysicalProperties = function(Next)
 		local Props = {}
-		local Value = Next()
+		local Value = assertType("Tag", Next())
 		repeat
-			Props[Value] = Next()
-			Next()
-			Value = Next()
+			Props[Value] = assertType("Literal", Next())
+			assertTag("/" .. Value, Next())
+			Value = assertType("Tag", Next())
 		until Value == "/PhysicalProperties"
 		return Props
 	end;
@@ -322,22 +345,24 @@ local TypeProcess = setmetatable({
 		B = B - R * 0x00010000
 		local G = (B - B % 0x00000100) / 0x00000100
 		B = B - G * 0x00000100
-		assert(Next() == "/Color3")
+		assertTag("/Color3", Next())
 		return "Color3.fromRGB(" .. R .. ", " .. G .. ", " .. B .. ")"
 	end;
 
 	Content = function(Next)
-		local Value = Next()
+		local Value = assertType("Tag", Next())
 		local v
 		if Value == "url" then
-			v = tostring(Next())
+			v = tostring((assertType("Literal", Next())))
 		elseif Value == "null" then
 			v = nil
 		else
 			error("No implementation for Content = " .. Value)
 		end
-		assert(Next() == "/" .. Value)
-		assert(Next() == "/Content")
+
+		assertTag("/" .. Value, Next())
+		assertTag("/Content", Next())
+
 		return v
 	end;
 
@@ -347,16 +372,16 @@ local TypeProcess = setmetatable({
 
 	Vector2 = ExpectFields("Vector2", {"X", "Y"});
 	UDim2 = ExpectFields("UDim2", {"XS", "XO", "YS", "YO"});
+
 	Rect2D = function(Next, Closer)
 		local x = {"min", "max"}
 		local t = {"X", "Y"}
 
-		local n = 4
 		local r = {}
 
-		for a = 1, 2 do
+		for a = 1, #x do
 			assertTag(x[a], Next())
-			for i = 1, 2 do
+			for i = 1, #t do
 				assertTag(t[i], Next())
 				r[i] = assertType("Literal", Next())
 				assertTag("/" .. t[i], Next())
@@ -367,29 +392,26 @@ local TypeProcess = setmetatable({
 		return "Rect.new(" .. table.concat(r, ", ") .. ")"
 	end;
 
-	ColorSequence = function(Next, Closer)
-		local Value, Type = Next()
-		assertType("Literal", Value, Type)
-
-		local Parameters = {}
-
-		for Num in Value:gmatch("%S+") do
-			table.insert(Parameters, Num)
-		end
-		assertTag(Closer, Next())
-		return "ColorSequence.new{" .. table.concat(Parameters, ", ") .. "}"
+	ColorSequence = function(...)
+		return "ColorSequence.new{" .. table.concat(GetSpaceSeparatedLiterals(...), ", ") .. "}"
 	end;
 
-	NumberRange = ExpectSpaceSeparatedLiterals("NumberRange");
-	NumberSequence = ExpectSpaceSeparatedLiterals("NumberSequence");
+	NumberRange = function(...)
+		return "NumberRange.new(" .. table.concat(GetSpaceSeparatedLiterals(...), ", ") .. ")"
+	end;
+
+	NumberSequence = function(...)
+		return "NumberSequence.new(" .. table.concat(GetSpaceSeparatedLiterals(...), ", ") .. ")"
+	end;
 }, {
-	__index = function(self, i)
+	__index = function(_, i)
 		error(i .. " needs an implementation")
 	end;
 })
 
 local EnterObject
 
+-- If the current Value starts with "Item class=", start interpreting it
 local function EnterObjectIfClass(Value, Objects, Next)
 	if Value then
 		local ClassName = MatchBalanced(Value, "Item class=%b\"\"")
@@ -402,6 +424,7 @@ local function EnterObjectIfClass(Value, Objects, Next)
 	end
 end
 
+-- Found an Object! Start interpreting its properties
 function EnterObject(ClassName, Parent, Next)
 	local self = {Children = {}; Parent = Parent; ClassName = ClassName}
 	table.insert(Parent.Children, self)
@@ -423,6 +446,7 @@ function EnterObject(ClassName, Parent, Next)
 	end
 end
 
+-- Constuct a Tree of all Objects in the game, (enter `Children`)
 local function MakeTree(Directory)
 	local Next = GetXMLParser(Directory)
 	local Objects = {Children = {}; ClassName = "DataModel"; Name = "src"}
@@ -430,6 +454,7 @@ local function MakeTree(Directory)
 	return Objects
 end
 
+-- Simple extendable command line program
 local Dash = ("-"):byte()
 
 local function Main(arc, argv)
@@ -455,8 +480,8 @@ local function Main(arc, argv)
 		print("Constructing Object tree from " .. Parameters[1])
 		print("This could take upwards of a minute for large files.")
 		local Objects = MakeTree(Parameters[1]:gsub("/", "\\"))
-		print("Complete Object tree. Took " .. (os.clock() - t1) .. " seconds.")
-		print("Instantiating Files at location " .. Parameters[2])
+		print("Completed Object tree. Took " .. (os.clock() - t1) .. " seconds.")
+		print("Instantiating Files at location: " .. Parameters[2])
 		BuildProject(Parameters[2]:gsub("/", "\\"), Objects)
 		print("Completed!")
 	end
